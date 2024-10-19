50f8b16b15151b98c49feeea6c05958295816563
Jef Brown <jefbrown@outlook.com>
2024-10-19 19:20:22 -0400

    flash enabled
    enabled Flash.nvim for jumping/hopping in a document.

--- lua/add/plugins.lua
+++ lua/add/plugins.lua
@@ -138,20 +138,20 @@ require('lazy').setup({
 		},
 	},
 	
-	-- { "folke/flash.nvim",
-	-- 	--search labels, enhanced character motions, and Treesitter integration.
-	-- 	event = "VeryLazy",
-	--  	---@type Flash.Config
-	--  	opts = {},
-	--  	-- stylua: ignore
-	--  	keys = {
-	--  		{ "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-	--    	{ "F", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-	--    	{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-	--    	{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-	--    	{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-	--  	},
-	-- },
+	{ "folke/flash.nvim",
+		--search labels, enhanced character motions, and Treesitter integration.
+		event = "VeryLazy",
+	 	---@type Flash.Config
+	 	opts = {},
+	 	-- stylua: ignore
+	 	keys = {
+	 		{ "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+	   	{ "F", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+	   	{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+	   	{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+	   	{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	 	},
+	},
 
 -- ┌┬┐┬┌┐┌┬
 -- ││││││││
