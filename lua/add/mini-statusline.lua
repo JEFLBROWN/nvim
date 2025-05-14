012607b8389c8a5df45d7f0a8cd138ac18657729
Jef Brown <jefbrown@outlook.com>
2025-05-14 18:52:47 -0400

    mini-statusline
    configured mini-statusline to simple display

--- lua/add/mini-statusline.lua
+++ lua/add/mini-statusline.lua
@@ -1,21 +1,26 @@
-local status_ok, statusline = pcall(require, 'statusline')
-if not status_ok then
-	return
-end
+require('mini.statusline').setup({
+  -- Whether to set Vim's settings for statusline (make it always visible)
+  set_vim_settings = true,  -- recommended
 
--- No need to copy this inside `setup()`. Will be used automatically.
-
-require ('statusline').setup {
-
-  -- Content of statusline as functions which return statusline string. See
-  -- `:h statusline` and code of default contents (used instead of `nil`).
+  -- Custom content for different sections
   content = {
-    -- Content for active window
-    active = nil,
-    -- Content for inactive window(s)
-    inactive = nil,
-  },
+    active = function()
+      local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+      local git           = MiniStatusline.section_git({ trunc_width = 75 })
+      local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+      local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+      local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+      local location      = MiniStatusline.section_location({ trunc_width = 75 })
 
-  -- Whether to use icons by default
-  use_icons = true,
-}
+      return MiniStatusline.combine_groups({
+        { hl = mode_hl,                 strings = { mode } },
+        { hl = 'MiniStatuslineDevinfo', strings = { git, } },
+        '%<', -- truncation point
+        { hl = 'MiniStatuslineFilename', strings = { filename } },
+				'%=',
+        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        -- { hl = 'MiniStatuslineLocation', strings = { location } },
+      })
+    end,
+  },
+})
